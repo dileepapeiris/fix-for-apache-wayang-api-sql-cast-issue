938de7b607f03c6d959b66ff01d5bdaecc93bc66
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2026-04-02 00:17:51 +0530

    Add SqlCastFilterDemoTest skeleton
    Introduce a new JUnit test file SqlCastFilterDemoTest under org.apache.wayang.api.sql.demo. The file adds imports and scaffolding for testing SQL cast and filter predicate behavior using Apache Calcite types and Wayang's FilterPredicateImpl (includes author header).

---
+++ src/test/java/org/apache/wayang/api/sql/demo/SqlCastFilterDemoTest.java
@@ -0,0 +1,19 @@
+/*
+ * Dileepa Peiris - [https://github.com/dileepapeiris] 
+ */
+
+package org.apache.wayang.api.sql.demo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.apache.calcite.jdbc.JavaTypeFactoryImpl;
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rex.RexBuilder;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.sql.fun.SqlStdOperatorTable;
+import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.wayang.api.sql.calcite.converter.functions.FilterPredicateImpl;
+import org.apache.wayang.basic.data.Record;
+import org.junit.jupiter.api.Test;
+

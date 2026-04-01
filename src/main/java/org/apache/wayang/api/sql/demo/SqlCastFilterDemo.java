da2323aa9cf4874757b9dd19be7c56250547c70c
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2026-04-02 00:16:44 +0530

    Add Javadoc for SqlCastFilterDemo
    Add a class-level Javadoc to SqlCastFilterDemo documenting a minimal end-to-end example of using FilterPredicateImpl with a CAST (integer -> VARCHAR) in the predicate and providing the Maven command to run the demo.

--- src/main/java/org/apache/wayang/api/sql/demo/SqlCastFilterDemo.java
+++ src/main/java/org/apache/wayang/api/sql/demo/SqlCastFilterDemo.java
@@ -12,3 +12,11 @@ import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.wayang.api.sql.calcite.converter.functions.FilterPredicateImpl;
 import org.apache.wayang.basic.data.Record;
+
+/**
+ * Minimal end-to-end use of {@link FilterPredicateImpl} with a {@code CAST} in the predicate
+ * (integer column to {@code VARCHAR}, compared to a literal). Run with:
+ * {@code mvn -pl wayang-api/wayang-api-sql-cast-demo exec:java}
+ */
+public final class SqlCastFilterDemo {
+}
